69c19c9473b5b999ee670b74184f1883cdc1af05
itani0997 <isla9190@student.acitech.org>
2025-11-18 12:49:30 -0500

    jk

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,15 +1,62 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int playerWins = 0;
+        int cpuWins = 0;
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int round = 1;
+        while(playerWins < 3 && cpuWins < 3) {
+            deck gameDeck = new deck();
+            gameDeck.builddeck();
+            gameDeck.shuffledeck();
+            System.out.println("ROUND " + round);
+            System.out.println("There are " + gameDeck.getCardsRemaining() + " cards in the deck.");
+            System.out.print("Choose a card position (1-" + (gameDeck.getCardsRemaining() - 2) + "): ");
+            int playerChoice = scanner.nextInt() - 1;
+            
+            // Player's turn
+            Card playerCard = gameDeck.removeCard(playerChoice);
+            System.out.println("You cut: " + playerCard);
+            // CPU's turn
+            int remainingCards = gameDeck.getCardsRemaining() - 1;
+            int cpuChoice = random.nextInt(remainingCards);
+            Card cpuCard = gameDeck.removeCard(cpuChoice);
+            System.out.println("CPU cuts: " + cpuCard);
+            
+            // Determine winner
+            System.out.println("\nComparing cards...");
+            if(playerCard.getFace() > cpuCard.getFace()) {
+                playerWins++;
+                System.out.println("You win this round!");
+            } else if(cpuCard.getFace() > playerCard.getFace()) {
+                cpuWins++;
+                System.out.println("CPU wins this round!");
+            } else {
+                // Tie - compare suits (Spades=1 > Hearts=2 > Clubs=3 > Diamonds=4)
+                if(playerCard.getSuit() < cpuCard.getSuit()) {
+                    playerWins++;
+                    System.out.println("You win by suit priority!");
+                } else {
+                    cpuWins++;
+                    System.out.println("CPU wins by suit priority!");
+                }
+            }
+            System.out.println("Current Score - Player: " + playerWins + " | CPU: " + cpuWins);
+            System.out.println("----------------------------------------\n");
+            round++;
         }
+
+        // Final results
+        System.out.println("=== FINAL RESULTS ===");
+        System.out.println("Player: " + playerWins + " | CPU: " + cpuWins);
+        if(playerWins > cpuWins) {
+            System.out.println("Congratulations! You won the match!");
+        } else {
+            System.out.println("CPU won the match. Better luck next time!");
+        }
+        scanner.close();
     }
 }
